ce768ad3ae74725587b6cdd97f7c2dd3debe8d6d
MatthijsdW <m.f.dewachter@gmail.com>
2021-11-04 10:45:40 +0100

    Initial commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float rotateSpeed;
+    public int coinValue;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Player>().AddScore(coinValue);
+            Destroy(gameObject);
+        }
+    }
+}
